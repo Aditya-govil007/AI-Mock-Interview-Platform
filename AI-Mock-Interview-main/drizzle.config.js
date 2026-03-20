a571cc5c0be3a99421c0837663631da93297f406
Aditya-govil007 <adityagovil2004@gmail.com>
2026-03-20 15:53:08 +0530

    Removed extra folder and cleaned structure

--- AI-Mock-Interview-main/drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import {"drizzle-kit"}.Config} */
-export default {
-    schema: "./utils/schema.js",
-    dialect: "postgresql",
-    dbCredentials: {
-        url: 'postgresql://neondb_owner:npg_A6F9lqnMfxOH@ep-wispy-mode-a5qs46pc-pooler.us-east-2.aws.neon.tech/Ai-mocker?sslmode=require',
-    }
-};
